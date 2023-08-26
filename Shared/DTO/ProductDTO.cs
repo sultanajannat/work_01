5cf084032c6711bbe4762fc1b5a7aa138a64030f
sultanajannat <sultanajannat94@gmail.com>
2023-08-26 11:46:39 +0600

    created a product list  razor page

---
+++ Shared/DTO/ProductDTO.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using work_01.Shared.Models;
+
+namespace work_01.Shared.DTO
+{
+	public class ProductDTO
+	{
+		public int ProductID { get; set; }
+
+		public string ProductName { get; set; } = default!;
+
+		//public string ProductDescription { get; set; }
+
+		//public string ProductCategoryID { get; set;}
+
+		//public string ProductCategoryName { get; set;}
+
+		//public string ProductCategoryDescription { get; set;}
+		[Required, Column(TypeName = "money"), DisplayFormat(DataFormatString = "{0:0.00}")]
+		public decimal Price { get; set; }
+		[Required, StringLength(150)]
+		public string Picture { get; set; } = default!;
+		public bool IsAvailable { get; set; }
+
+		
+
+	}
+}
