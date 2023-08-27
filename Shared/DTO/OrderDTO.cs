6cd70e160069dca11d69b2320bddaf1b6f18afea
sultanajannat <sultanajannat94@gmail.com>
2023-08-27 11:38:17 +0600

    product edit interface has been done

---
+++ Shared/DTO/OrderDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work_01.Shared.DTO
+{
+	public class OrderDTO
+	{
+
+	}
+}
---
+++ Shared/DTO/ProductEditDTO.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work_01.Shared.DTO
+{
+	public class ProductEditDTO
+	{
+		public int ProductID { get; set; }
+
+		public string ProductName { get; set; } = default!;
+
+		
+		[Required, Column(TypeName = "money"), DisplayFormat(DataFormatString = "{0:0.00}")]
+		public decimal Price { get; set; }
+		[Required, StringLength(150)]
+		public string Picture { get; set; } = default!;
+		public bool IsAvailable { get; set; }
+	}
+}
